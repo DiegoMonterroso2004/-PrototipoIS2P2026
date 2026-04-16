da546e30fe7567f4bac1c4cd04f78904fb5e6ca4
221369 - DIEGO ANDRE MONTERROSO RABARIQUE <dmonterrosor5@miumg.edu.gt>
2026-04-16 11:04:09 -0600

    Usuario con permisos

---
+++ codigo/empresarial/MRP - G1/Mantenimientos/Mantenimiento_examen/ExamenM2/Capa_vista_Mantenimiento/Frm_Mantenimiento_Peliculas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_vista_Mantenimiento
+{
+    public partial class Frm_Mantenimiento_Peliculas : Form
+    {
+        public Frm_Mantenimiento_Peliculas()
+        {
+            InitializeComponent();
+        }
+    }
+}
